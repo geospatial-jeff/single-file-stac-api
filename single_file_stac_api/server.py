11cdf0b0c727e39d9e8e8eec93374790ea16e27c
Jeff Albrecht <geospatialjeff@gmail.com>
2021-02-03 21:46:50 -0600

    add server

---
+++ single_file_stac_api/server.py
@@ -0,0 +1,22 @@
+"""single_file_stac_api.server"""
+import uvicorn
+from pydantic import BaseSettings
+from stac_api.api.app import StacApi, inject_settings
+
+from single_file_stac_api.backend import SingleFileClient
+
+
+def start_application(filename: str):
+    """start the application."""
+
+    class ApiSettings(BaseSettings):
+        """api settings."""
+
+        debug: bool = True
+        host: str = "localhost"
+        port: int = 8005
+
+    settings = ApiSettings()
+    inject_settings(settings)
+    api = StacApi(settings=settings, client=SingleFileClient.from_file(filename))
+    uvicorn.run(app=api.app, host=settings.host, port=settings.port, log_level="info")
